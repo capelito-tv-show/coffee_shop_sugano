77c884ca6056e7b0afa7633167d16886b9ebbddd
[capelito-tv-show] <capelito.tv.show@gmail.com>
2017-06-09 01:34:09 +0900

    production.rbの変更/mysqlのバージョン変更

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
+
+
 require 'rails/all'
 
 
@@ -23,5 +25,6 @@ module CoffeeShopSugano
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
